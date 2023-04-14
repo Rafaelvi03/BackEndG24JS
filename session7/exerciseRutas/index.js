e3c08de149467bad5fffe6a302bed1fe7eedbb64
Rafael <rafaelvi03111@gmail.com>
2023-04-13 20:30:44 -0500

    feat: Termino de ejercicio

--- session7/exerciseRutas/index.js
+++ session7/exerciseRutas/index.js
@@ -27,20 +27,62 @@ server.get('/koders', async (request, response) => {
         }
     })
 })
-server.post('/koders', (request, response) => {
-    const body = request.body
-    console.log(body)
-    response.json({ message: "Aqui se crearan Koders" })
+server.post('/koders', async (request, response) => {
+    const dataFile = await fs.promises.readFile("./koders.json", "utf8")
+    const json = JSON.parse(dataFile)
+    const koders = json.koders
+
+    const newKoder = request.body
+
+    json.koders.push(newKoder)
+
+    //json.koders = koders
+    await fs.promises.writeFile('./koders.json', JSON.stringify(json, null, 2), 'utf8')
+
+    console.log(newKoder)
+    const message = {
+        success: true,
+        message: "Se agrego un nuevo koder exitosamente!!"
+    }
+    response.json(message)
 })
 
-server.patch('/koders', (request, response) => {
+server.patch('/koders/:idkoder', async (request, response) => {
 
-    response.json({ message: "Aqui se actualizaran los koders" })
-})
-server.delete('/koders', (request, response) => {
+    const koderUpdated = request.body;
+    const dataFile = await fs.promises.readFile('./koders.json', 'utf8');
+    const json = JSON.parse(dataFile);
+    const koders = json.koders;
+    const koderFind = koders.find(koder => koder.id === koderUpdated.id)
+    koders.splice(koderFind, 1, koderUpdated);
+    await fs.promises.writeFile('./koders.json', JSON.stringify(json, null, 2), 'utf8');
 
-    response.json({ message: "Aqui se borraran los koders" })
+    response.json({
+        success: true,
+        message: 'Koder actualizado!',
+        data: {
+            koder: koderUpdated
+        }
+    })
+
 })
+
+server.delete('/koders/:idKoder', async (request, response) => {
+    const { idKoder } = request.params
+    console.log(typeof idKoder)
+    const dataFile = await fs.promises.readFile('./koders.json', 'utf8')
+    const json = JSON.parse(dataFile)
+    const koders = json.koders;
+    const newKoder = koders.filter(koder => koder.id !== parseInt(idKoder))
+    json.koders = newKoder;
+    await fs.promises.writeFile('./koders.json', JSON.stringify(json, null, 2), 'utf8')
+    response.json({
+        success: true,
+        message: 'Koder eliminado!',
+
+    })
+})
+
 
 server.listen(port, () => {
     console.log(`Servidor corriendo inicalizado en el puerto ${port}`)
